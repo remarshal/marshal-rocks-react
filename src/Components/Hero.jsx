24c3f320b72db46daf7b3cec97eb4c39565e9285
remarshal <marshalwaldendev@gmail.com>
2022-04-05 18:00:39 -0400

    hero

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,10 +1,15 @@
-import './hero.css'
+import './hero.css';
 
 const Hero = () => {
     return (
-        <div>
-            <h1>Hero</h1>
-        </div>
+        <header className='hero--container'>
+            <div className="container">
+                <h1 className='hero--text'>Marshal <span>Rocks</span></h1>
+                <h5 className='hero--text'>As a Developer</h5>
+                <a href="48:45" className='btn hero--btn'>Download Resume</a>
+                <a href="#contact" className='btn btn-primary hero--btn'>Let's Talk</a>
+            </div>
+        </header>
     );
 };
 
