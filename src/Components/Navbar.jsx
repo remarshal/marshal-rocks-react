21caa8f1546ba001d4c69b56645f6af61c024d6e
remarshal <marshalwaldendev@gmail.com>
2022-04-05 19:53:15 -0400

    navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,19 @@
-import './navbar.css'
+import './navbar.css';
+import { AiOutlineHome } from 'react-icons/ai';
+import { BsPerson, BsGrid } from 'react-icons/bs';
+import { MdOutlineEmail } from 'react-icons/md';
 
 const Navbar = () => {
     return (
-        <div>
-            
+        <div id="navbar">
+            <nav className='nav-menu'>
+                <ul>
+                    <li><a href="#hero" className="active"><AiOutlineHome /><i class="bx bx-home"></i> <span>Home</span></a></li>
+                    <li><a href="#about" ><BsPerson /><i class="bx bx-user"></i> <span>About</span></a></li>
+                    <li><a href="#portfolio" ><BsGrid /><i class="bx bx-book-content"></i> <span>Portfolio</span></a></li>
+                    <li><a href="#contact" ><MdOutlineEmail /><i class="bx bx-envelope"></i> <span>Contact</span></a></li>
+                </ul>
+            </nav>
         </div>
     );
 };
