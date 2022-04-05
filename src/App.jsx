7ee58d0eb21df94e0509c786cd2d4a092f1b91d7
remarshal <marshalwaldendev@gmail.com>
2022-04-05 14:21:42 -0400

    Components in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from "react";
+import Hero from "./Components/Hero";
+import Navbar from "./Components/Navbar";
+import About from "./Components/About";
+import Portfolio from "./Components/Portfolio";
+import Footer from "./Components/Footer";
 
 const App = () => {
     return (
         <div>
-            
+            <Hero />
+            <Navbar />
+            <About />
+            <Portfolio />
+            <Footer />
         </div>
     );
 };
---
+++ src/Components/Portfolio.jsx
@@ -0,0 +1,11 @@
+import './portfolio.css'
+
+const Portfolio = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Portfolio;
