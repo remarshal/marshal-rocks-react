31ed48bffdaf3405c166c40f0220cdd6e64496f4
remarshal <marshalwaldendev@gmail.com>
2022-04-06 11:24:56 -0400

    hero links

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -8,11 +8,11 @@ const Hero = () => {
                 <h1 className='hero--text'>Marshal <span>Rocks</span></h1>
                 <h5 className='hero--text'>As a Developer</h5>
                 <a href="48:45" className='btn hero--btn'>Download Resume</a>
-                <a href="#contact" className='btn btn-primary hero--btn'>Let's Talk</a>
+                <a href="#contact" className='btn btn-primary hero--btn'>Let Chat!</a>
                 <div className='hero--social'>
-                    <a href="https://linkedin.com" target="_blank"><BsLinkedin /></a>
-                    <a href="https://github.com" target="_blank"><BsGithub /></a>
-                    <a href="https://instagram.com" target="_blank"><BsInstagram /></a>
+                    <a href="https://linkedin.com/in/remarshal" target="_blank"><BsLinkedin /></a>
+                    <a href="https://github.com/remarshal" target="_blank"><BsGithub /></a>
+                    <a href="https://instagram.com/remarshal" target="_blank"><BsInstagram /></a>
                 </div>
             </div>
         </header>
