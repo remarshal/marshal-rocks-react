0758babc9b2e1e4df3697e3cf0222f2383fd50f2
remarshal <marshalwaldendev@gmail.com>
2022-04-08 14:24:09 -0400

    portfolio and card css

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,17 +1,18 @@
-import React from 'react';
 import './card.css';
 
 const Card = (props) => {
     return (
         <div>
-            <article className="portfolio--item" key={props.project.id}>
-                <div className="portfolio--img">
-                    <img src={props.project.image} alt="Portfolio Site Image" />
+            <article className="cards" key={props.project.id}>
+                <div className="card--img">
+                    <img src={props.project.image} alt={props.project.title} />
                 </div>
-                <h3>{props.project.title}</h3>
-                <h5>{props.project.type}</h5>
-                <a href={props.project.github} target='_blank' className='btn'>Github</a>
-                <a href={props.project.site} target='_blank' className='btn btn-primary'>Live Site</a>
+                <div className='card--info'>
+                    <h3>{props.project.title}</h3>
+                    <h5>{props.project.type}</h5>
+                    <a href={props.project.github} target='_blank' className='btn'>Github</a>
+                    <a href={props.project.site} target='_blank' className='btn btn-primary'>Live Site</a>
+                </div>
             </article>
         </div>
     );
