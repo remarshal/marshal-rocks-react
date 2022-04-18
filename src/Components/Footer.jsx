ad0149ec583dd83c4c52b326649abc075fe6739f
remarshal <marshalwaldendev@gmail.com>
2022-04-18 18:37:27 -0400

    footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,23 @@
-import './footer.css'
+import './footer.css';
+import { BsLinkedin, BsGithub, BsInstagram, BsFacebook } from 'react-icons/bs';
 
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <footer>
+            <a href='#'><h1>Marshal <span>Rocks</span></h1></a>
+            <div className='footer--social'>
+                <a href="https://instagram.com/marshal.rocks" target="_blank"><BsInstagram /></a>
+                <a href="https://facebook.com/marshal.rocks" target="_blank"><BsFacebook /></a>
+                <a href="https://linkedin.com/in/remarshal" target="_blank"><BsLinkedin /></a>
+                <a href="https://github.com/remarshal" target="_blank"><BsGithub /></a>
+            </div>
+            <div className="copyright">
+                &copy; Copyright <strong><span>2022</span></strong>. All Rights Reserved
+            </div>
+            <div className="credits">
+                <a href="#">marshal.rocks</a>
+            </div>
+        </footer>
     );
 };
 
