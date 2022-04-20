b65094daa7491840e5f03a7399bf494c2ea23536
remarshal <marshalwaldendev@gmail.com>
2022-04-20 12:16:22 -0400

    added state & effect to hide scroll button

--- src/Components/ScrollTop.jsx
+++ src/Components/ScrollTop.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import './scrolltop.css'
 import {BsArrowUpCircle} from 'react-icons/bs'
+import { useEffect, useState } from 'react';
 
 const ScrollTop = () => {
+    const [hidden, setHidden] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener('scroll', listenToScroll)
+        return () =>
+            window.removeEventListener('scroll', listenToScroll)
+    }, [])
+    
+    const listenToScroll = () => {
+        let heightToShow = 1000
+        const winScroll = document.body.scrollTop || document.documentElement.scrollTop
+
+        winScroll < heightToShow ? setHidden(false) : setHidden(true)
+    }
+
     return (
         <div className="scrolltop" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
-            <BsArrowUpCircle />
+            {hidden && <BsArrowUpCircle />}
         </div>
     );
 };
