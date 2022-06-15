55f53e13506051211e368f072f925e1392cb5486
remarshal <marshalwaldendev@gmail.com>
2022-06-15 12:56:52 -0400

    updated images

--- src/portfolio-data.js
+++ src/portfolio-data.js
@@ -2,49 +2,39 @@ export default [
   {
     id: 0,
     title: "Water Color Painting",
-    type: "Wordpress",
+    type: "Wordpress Website",
     description:
       "I created this site for my personal watercolor painting hobby.  It taught me not only Wordpress, but how to manage the domain, install a SSL certificate, deployment with backups, and security practices.",
-    image: "../images/portfolio/portfolio-7.png",
+    image: "../images/portfolio/portfolio-0.png",
     github: "https://github.com/remarshal/",
     site: "https://marshalwalden.com/",
     showsite: true,
   },
+
   {
     id: 1,
-    title: "Campsite Reservations",
-    type: "React Class Project",
-    description:
-      "To demonstrate the differences between Bootstrap and React, our class project was to recreate the fictional campsite using React.  Having components makes managing larger website much more manageable!",
-    image: "../images/portfolio/portfolio-6.png",
-    github: "https://github.com/remarshal/React-NuCamp",
-    site: "https://remarshal-nucamp-react.netlify.app/",
-    showsite: true,
-  },
-  {
-    id: 2,
     title: "Campsite Reservations",
     type: "Bootstrap Class Project",
     description:
       "In additional to our case study project, all of us in class collaborated together on this fictional campsite website while learning and applying our new Bootstrap skills.",
-    image: "../images/portfolio/portfolio-5.png",
+    image: "../images/portfolio/portfolio-1.png",
     github: "https://github.com/remarshal/newcamp-site",
     site: "https://remarshal-nucamp-bootstrap.netlify.app/",
     showsite: true,
   },
   {
-    id: 3,
+    id: 2,
     title: "Travel Journal",
-    type: "React",
+    type: "React Webpage",
     description:
       "This was my Initial project learning the basics of React while applying the skills developed in our earlier classes of HTML, CSS, and JavaScript.  Overall, a good foundational project.",
-    image: "../images/portfolio/portfolio-4.png",
+    image: "../images/portfolio/portfolio-2.png",
     github: "https://github.com/remarshal/travel-journal",
     site: "https://remarshal-travel-journal.netlify.app/",
     showsite: true,
   },
   {
-    id: 4,
+    id: 3,
     title: "Pottery Studio Store",
     type: "Bootstrap Ecommerce",
     description:
@@ -55,12 +45,23 @@ export default [
     showsite: true,
   },
   {
+    id: 4,
+    title: "Campsite Reservations",
+    type: "React Class Project",
+    description:
+      "To demonstrate the differences between Bootstrap and React, our class project was to recreate the fictional campsite using React.  Having components makes managing larger website much more manageable!",
+    image: "../images/portfolio/portfolio-4.png",
+    github: "https://github.com/remarshal/React-NuCamp",
+    site: "https://remarshal-nucamp-react.netlify.app/",
+    showsite: true,
+  },
+  {
     id: 5,
     title: "Color Game",
     type: "Vanilla JavaScript",
     description:
       "I built this color guessing game using vanilla JavaScript.  It demonstrates my ability to combine and apply skills learned through various lessons.  And it's fun to play!",
-    image: "../images/portfolio/portfolio-2.png",
+    image: "../images/portfolio/portfolio-5.png",
     github: "https://github.com/remarshal/color-game",
     site: "https://remarshal.github.io/color-game/",
     showsite: true,
@@ -71,7 +72,7 @@ export default [
     type: "React App",
     description:
       "Don't let the simplicity fool you, this single page app packs a lot under the hood!  For example, the randomly generated meme images make use of Math and an API connection.  While the text is captured using State and Effect to apply over the image.",
-    image: "../images/portfolio/portfolio-1.png",
+    image: "../images/portfolio/portfolio-6.png",
     github: "https://github.com/remarshal/meme-generator",
     site: "https://remarshal-meme-generator.netlify.app/",
     showsite: true,
@@ -82,7 +83,7 @@ export default [
     type: "React App",
     description:
       "This fast-paced game is enjoyable and very simple.  It was created in React 18.0.0 to continue improving my skills while retaining my newly acquired knowledge.  It utilizes passing props, receiving values from children, ternary operators, useState, useEffect, and a fun surprise when you win!",
-    image: "../images/portfolio/portfolio-0.png",
+    image: "../images/portfolio/portfolio-7.png",
     github: "https://github.com/remarshal/tenzies-game",
     site: "https://remarshal-tenzies-game.netlify.app/",
     showsite: true,
