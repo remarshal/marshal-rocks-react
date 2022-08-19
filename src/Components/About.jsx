1b06c00353cf13fd52face00dac5305d1f9ad8fb
remarshal <marshalwaldendev@gmail.com>
2022-08-19 12:52:19 -0400

    updated typo

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -38,7 +38,7 @@ const About = () => {
                 </div>
                 <div className="about--content">
                     <h4>Results-driven Front-End Developer</h4>
-                    <p>I'm recently graducated from the <a href='https://nucamp.co' target='_blank'>Full-Stack Web + Mobile Development</a>bootcamp from <a href='https://nucamp.co' target='_blank'>NuCamp.</a>I'm proud of all 5 certifications displayed above!  The focus has been predominately front end with React, with the final one working with back-end servers in Node.js and using MongoDB databases.</p>
+                    <p>I'm recently graduated from the <a href='https://nucamp.co' target='_blank'>Full-Stack Web and Mobile Development</a>bootcamp at <a href='https://nucamp.co' target='_blank'>NuCamp.</a>I'm proud of all 5 certifications displayed above!  My focus has been predominately front end learning React, with our final certification on back-end servers using MongoDB databases and Node JS.</p>
                     <p>I have a passion bringing the technical and creative aspects of web development to each project. I work to bridge these together thereby creating a user experience to the design. Connecting and interacting with a website encourages more community engagement.</p>
                     <p>I am happiest when creating, learning, and exploring how to bring websites to life. My experience managing systems, networks, and IT administration puts me in a unique position to troubleshoot and solve problems on my coding journey.  Occasionally I'm available for freelance work, feel free to reach out and say hi!</p>
                     <a href='#contact' className='btn btn-primary'>Let's connect!</a>
