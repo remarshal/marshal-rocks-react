b3c86478bdc94b09490194ae6e7ed9d5bec925b1
remarshal <marshalwaldendev@gmail.com>
2023-01-03 15:53:13 -0500

    updating current projects

--- src/Components/Journey.jsx
+++ src/Components/Journey.jsx
@@ -9,9 +9,12 @@ const Journey = () => {
                 <h2>My Journey</h2>
                 <h5>What I'm currently working on</h5>
             </div>
+            
             <div className="container journey--container">
                 <div className="journey--content">
-                    <p>Improving my skills in React and vanilla JavaScript.  Also having fun with <a href='https://instagram.com/marshal.rocks' target='_blank'>#100DaysOfCode</a>challenge while building and creating projects.</p>
+                    <p>AWS Certified Solutions Architect Associate SAA-C03.</p>
+                    <p>I'm currently working toward my <a href='https://aws.amazon.com/certification/certified-solutions-architect-associate/' target='_blank'>AWS Certified Solutions Architect Certification</a>through the online platform at <a href='https://acloudguru.com/course/aws-certified-solutions-architect-associate-saa-c03' target='_blank'>A Cloud Guru.</a>  Accenture is committed to my growth, and offering this amazing opportunity to gain another certification.  It's one of the most valuable and sought-after cloud computing certifications. </p>
+                    <p>Also having fun on <a href='https://instagram.com/marshal.rocks' target='_blank'>Instagram</a>sharing my journey and coding projects.</p>
                     <a href="https://instagram.com/marshal.rocks" target="_blank" className='btn btn--journey'>Follow my journey</a>
                     <a href='#contact' className='btn btn-primary btn--journey'>Let's connect!</a>
                 </div>
