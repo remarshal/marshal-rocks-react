228cb9217ce62b932cd2e1ca4fbabfc3849ec6b7
remarshal <marshalwaldendev@gmail.com>
2023-01-03 15:59:22 -0500

    added AWS badge

--- src/Components/Journey.jsx
+++ src/Components/Journey.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './journey.css';
-import ChallengeImg from '../assets/img/challenge.png';
+import AWSBadge from '../assets/img/aws-certified-solutions-architect-associate_badge.png';
 
 const Journey = () => {
     return (
@@ -9,18 +9,19 @@ const Journey = () => {
                 <h2>My Journey</h2>
                 <h5>What I'm currently working on</h5>
             </div>
-            
+
             <div className="container journey--container">
                 <div className="journey--content">
                     <p>AWS Certified Solutions Architect Associate SAA-C03.</p>
+                    <div className="journey--section">
+                        <img src={AWSBadge} alt="AWS Certified Solutions Architect Associate SAA-C03 badge" className='journey--img' />
+                    </div>
                     <p>I'm currently working toward my <a href='https://aws.amazon.com/certification/certified-solutions-architect-associate/' target='_blank'>AWS Certified Solutions Architect Certification</a>through the online platform at <a href='https://acloudguru.com/course/aws-certified-solutions-architect-associate-saa-c03' target='_blank'>A Cloud Guru.</a>  Accenture is committed to my growth, and offering this amazing opportunity to gain another certification.  It's one of the most valuable and sought-after cloud computing certifications. </p>
-                    <p>Also having fun on <a href='https://instagram.com/marshal.rocks' target='_blank'>Instagram</a>sharing my journey and coding projects.</p>
+
+                    {/* <p>Also having fun on <a href='https://instagram.com/marshal.rocks' target='_blank'>Instagram</a>sharing my journey and coding projects.</p>
                     <a href="https://instagram.com/marshal.rocks" target="_blank" className='btn btn--journey'>Follow my journey</a>
-                    <a href='#contact' className='btn btn-primary btn--journey'>Let's connect!</a>
+                    <a href='#contact' className='btn btn-primary btn--journey'>Let's connect!</a> */}
                 </div>
-            </div>
-            <div className="journey--section">
-                <img src={ChallengeImg} alt="Challenge Rules" className='journey--img' />
             </div>
         </section>
     );
